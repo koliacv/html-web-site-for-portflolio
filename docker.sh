c9aac1000997ff62686e2b56ac744cc6f9f75a1d
Kolia Palahniuk <koliapalahniuk@gmail.com>
2022-09-01 23:02:29 +0300

    01.09.2022

---
+++ docker.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+#           Run dockerfile in git repository koliacv/website 
+#
+#    This script will run only if docker and git installed on machine 
+#
+#
+git clone https://github.com/koliacv/website.git
+echo "cd website/"
+cd website/
+docker build -t website .
+echo "docker builded"
+docker run -d -p 80:80 website
