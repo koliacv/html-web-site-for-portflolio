bdd4c79bda43cd61de2e907c566497a3b915283f
Kolia Palahniuk <71904359+koliacv@users.noreply.github.com>
2022-09-05 14:17:36 +0300

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -5,7 +5,7 @@
 #    This script will run only if docker and git installed on machine 
 #
 #
-cd website/
+
 docker build -t website .
 echo "docker builded"
 docker run -d -p 80:80 website
@@ -13,4 +13,3 @@ echo "Docker run and work then"
 MYIP1=$(hostname -I | awk '{print $1}')
 MYIP2=$(hostname -I | awk '{print $2}')
 echo "go to ${MYIP1}:80 or ${MYIP2}:80"
-apt-get install grep -y
