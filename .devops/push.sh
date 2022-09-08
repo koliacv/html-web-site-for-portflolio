6089c438a276d018586d7557e56a24bdc43531b2
Kolia Palahniuk <koliapalahniuk@gmail.com>
2022-09-08 14:43:33 +0300

    Add CI/CD

--- .devops/push.sh
+++ .devops/push.sh
@@ -5,6 +5,9 @@
 #    This script will run only if docker and git installed on machine 
 #
 #
+cd .. | cd ..
+rm -rf website/ 
+mkdir devops | cd devops
 apt install wget -y
 wget https://raw.githubusercontent.com/koliacv/website/master/.devops/docker-compose.yml
 pwd
