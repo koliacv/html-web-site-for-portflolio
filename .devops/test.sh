55bfa2029092a1dbed30fa384ee66d6987ef606f
Kolia Palahniuk <koliapalahniuk@gmail.com>
2022-09-08 13:03:35 +0300

    Add CI/CD

--- .devops/test.sh
+++ .devops/test.sh
@@ -6,11 +6,17 @@
 #
 #
 echo "cd website/.devops/"
-docker build -t website .
+docker build -t kolia2526/website .
 echo "docker builded"
-docker run -d -p 80:80 website
+docker run -d -p 80:80 kolia2526/website
 echo "Docker run and work then"
 MYIP1=$(hostname -I | awk '{print $1}')
 MYIP2=$(hostname -I | awk '{print $2}')
 echo "go to ${MYIP1}:80 or ${MYIP2}:80"
+
+echo "Test docker continer"
+pwd=$(pwd)
+ls -lah
+docker ps -a|awk '{print $1}'|grep -v "CONTAINER"|grep -m1 "" > id.txt
+docker exec -i $(cat $pwd/id.txt) ls -lah /usr/share/nginx/html
 
