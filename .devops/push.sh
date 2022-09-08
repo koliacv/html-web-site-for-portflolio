132946ed924968007f7aaafeedba412a9eeff570
Kolia Palahniuk <koliapalahniuk@gmail.com>
2022-09-08 15:18:02 +0300

    Add CI/CD

---
+++ .devops/push.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+#           Run dockerfile in git repository koliacv/website 
+#
+#    This script will run only if docker and git installed on machine 
+#
+#
+pwd
+docker-compose up -d 
