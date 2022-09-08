25ade016db75ec787e31f608f73c7d34de5e71fc
Kolia Palahniuk <koliapalahniuk@gmail.com>
2022-09-08 13:40:38 +0300

    Add CI/CD

--- .devops/test.sh
+++ .devops/test.sh
@@ -5,21 +5,29 @@
 #    This script will run only if docker and git installed on machine 
 #
 #
-echo "cd website/.devops/"
-docker build . --file Dockerfile -t kolia2526/website
-echo "docker builded"
+#ENV
+REPO_NAME=kolia2526/website
+pwd=$(pwd)
+###
+echo "---------------------------------Start docker build------------------------------"
+docker build . --file Dockerfile -t $REPO_NAME
+echo "docker builded--------------------------"
 echo "--------------------------------DOCKER RUN-------------------------------"
 docker kill $(docker ps -q)
-docker run -d -p 80:80 kolia2526/website
+docker run -d -p 80:80 $REPO_NAME
 echo "Docker run and work then" 
 MYIP1=$(hostname -I | awk '{print $1}')
 MYIP2=$(hostname -I | awk '{print $2}')
-echo "go to ${MYIP1}:80 or ${MYIP2}:80"
+echo "--------------------go to ${MYIP1}:80 or ${MYIP2}:80---------------------------"
 
-echo "Test docker continer"
-pwd=$(pwd)
+echo "---------------------------Test docker continer--------------------------------"
+
 ls -lah
 docker ps -a|awk '{print $1}'|grep -v "CONTAINER"|grep -m1 "" > id.txt
 cat $pwd/id.txt
 docker exec -i $(cat $pwd/id.txt) ls -lah /usr/share/nginx/html
 
+
+
+echo "---------------------------PUSH TO DOCKERHUB-----------------------------------"
+docker push $REPO_NAME:$(echo "$(date +'%d-%m-%Y--%H-%M')")
