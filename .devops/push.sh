cb66363095660eecbce6ed695141f814f50958f8
Kolia Palahniuk <koliapalahniuk@gmail.com>
2022-09-09 16:52:06 +0300

    Add CI/CD

--- .devops/push.sh
+++ .devops/push.sh
@@ -5,5 +5,7 @@
 #    This script will run only if docker and git installed on machine 
 #
 #
+echo "---------------Start deploy to server---------------"
 pwd
+docker kill $(docker ps -q)
 docker-compose up -d 
