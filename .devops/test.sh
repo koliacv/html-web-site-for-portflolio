14cefe51765aac20a7ffab60954aa4e88942d39f
Kolia Palahniuk <koliapalahniuk@gmail.com>
2022-09-13 10:36:38 +0300

    Add CI/CD

--- .devops/test.sh
+++ .devops/test.sh
@@ -11,10 +11,12 @@ pwd=$(pwd)
 date=latest
 ###
 echo "---------------------------------Start docker build------------------------------"
+docker kill $(docker ps -q)
+docker rm $(docker ps -a -f status=exited -q)
 docker build . --file Dockerfile -t $REPO_NAME:$date
 echo "docker builded--------------------------"
 echo "--------------------------------DOCKER RUN-------------------------------"
-docker kill $(docker ps -q)
+
 docker run -d -p 80:80 $REPO_NAME:$date
 echo "Docker run and work then" 
 MYIP1=$(hostname -I | awk '{print $1}')
@@ -24,11 +26,16 @@ echo "--------------------go to ${MYIP1}:80 or ${MYIP2}:80----------------------
 echo "---------------------------Test docker continer--------------------------------"
 
 ls -lah
+echo "--------workdir------"
 docker ps -a|awk '{print $1}'|grep -v "CONTAINER"|grep -m1 "" > id.txt
 cat $pwd/id.txt
-docker exec -i $(cat $pwd/id.txt) ls -lah /usr/share/nginx/html
+
+echo "---------docker continer-------"
+
+docker exec -i $(cat $pwd/id.txt) ls /usr/share/nginx/html
 
 
 
 echo "---------------------------PUSH TO DOCKERHUB-----------------------------------"
+
 docker push $REPO_NAME:$date
